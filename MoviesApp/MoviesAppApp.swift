f09c86202f1a29164aafd5a770718ad6c47091a1
steingolditay <steingolditay@gmail.com>
2021-05-14 13:01:07 +0300

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Itay Steingold on 14/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
